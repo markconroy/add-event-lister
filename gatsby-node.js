afeaad1833bac2dadad5e7c6d211a98bd0e2e6c7
Mark Conroy <mark@mark.ie>
2020-04-06 20:39:11 +0100

    adding template to list events by country

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,15 +5,17 @@
  */
 
 const path = require(`path`)
+const _ = require("lodash")
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const eventPageTemplate = path.resolve(`src/templates/eventTemplate.js`)
+  const countryPageTemplate = path.resolve("src/templates/countryTemplate.js")
 
   const result = await graphql(`
     {
-      allMarkdownRemark(
+      eventsRemark: allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___start_date] }
       ) {
         edges {
@@ -22,6 +24,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
               path
             }
           }
+        }
+      }
+      countriesRemark: allMarkdownRemark {
+        group(field: frontmatter___country) {
+          fieldValue
         }
       }
     }
@@ -33,11 +40,23 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     return
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  const events = result.data.eventsRemark.edges
+  events.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.path,
       component: eventPageTemplate,
       context: {},
+    })
+  })
+
+  const countries = result.data.countriesRemark.group
+  countries.forEach(country => {
+    createPage({
+      path: `/countries/${_.kebabCase(country.fieldValue)}/`,
+      component: countryPageTemplate,
+      context: {
+        country: country.fieldValue,
+      },
     })
   })
 }
