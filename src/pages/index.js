06a8ba9109c729fd989921a12c111126c289b253
Mark Conroy <mark@mark.ie>
2020-04-07 18:39:51 +0100

    make the homepage listing more human readable

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ const IndexPage = ({ data }) => (
     <p>{data.allMarkdownRemark.edges.length} Upcoming Developer Events</p>
     <p>Browse:
       <ul>
-        <li><Link to="/events">All Events ({data.allMarkdownRemark.edges.length})</Link></li>
-        <li><Link to="/countries">By Country ({data.allMarkdownRemark.countriesGroup.length})</Link></li>
-        <li><Link to="/cities">By City ({data.allMarkdownRemark.cityGroup.length})</Link></li>
+        <li><Link to="/events">There are ({data.allMarkdownRemark.edges.length}) upcoming events</Link></li>
+        <li><Link to="/countries">In {data.allMarkdownRemark.countriesGroup.length} Countries</Link></li>
+        <li><Link to="/cities">Across {data.allMarkdownRemark.cityGroup.length} Cities</Link></li>
       </ul>
     </p>
 
