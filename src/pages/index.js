c38580e61ebd2f2342c65c71b91785895febe74d
Mark Conroy <mark@mark.ie>
2020-04-07 08:48:02 +0100

    turn index into a home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
-import React, { Fragment } from "react"
-import { graphql } from "gatsby"
+import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Card from "../components/card"
 import StyledHeading from "../components/global-styles/headings.js"
-import GridContainer from "../components/global-styles/grid-container.js"
-import GridItem from "../components/global-styles/grid-item.js"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -15,26 +12,13 @@ const IndexPage = ({ data }) => (
     <StyledHeading h1>Free Developer Events</StyledHeading>
 
     <p>{data.allMarkdownRemark.edges.length} Upcoming Developer Events</p>
-    <p>Click an event for more information.</p>
-    
-    <GridContainer>
-      {data.allMarkdownRemark.edges.map(edge => (
-        <Fragment>
-          <GridItem>
-            <Card
-              cardTitle = {edge.node.frontmatter.title}
-              cardPath = {edge.node.frontmatter.path}
-              cardCountry = {edge.node.frontmatter.country}
-              cardCity = {edge.node.frontmatter.city}
-              cardStartDate = {edge.node.frontmatter.start_date}
-              cardEndDate = {edge.node.frontmatter.end_date}
-              cardStartDateString = {edge.node.frontmatter.start_date_as_string}
-              cardEndDateString = {edge.node.frontmatter.end_date_as_string}
-            />
-          </GridItem>
-        </Fragment>
-      ))}
-    </GridContainer>
+    <p>Browse:
+      <ul>
+        <li><Link to="/events">All Events</Link></li>
+        <li><Link to="/countries">By Country</Link></li>
+        <li><Link to="/cities">By City</Link></li>
+      </ul>
+    </p>
 
   </Layout>
 )
