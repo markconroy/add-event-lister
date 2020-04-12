f8357f6eefe99b52d47689e6120a62eb63c6b235
Mark Conroy <mark@mark.ie>
2020-04-12 21:35:45 +0100

    make header a little more presentable

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,43 @@
 import { StaticQuery, graphql, Link } from "gatsby"
 import React from "react"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  border-bottom: 4px solid var(--primary);
+
+  .header__inner {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 1.5rem 1rem;
+    display: flex;
+    justify-content: space-between;
+    > * {
+      flex-basis: 50%;
+    }
+  }
+  .header__link {
+    text-decoration: none;
+    background-color: var(--primary);
+    display: inline-block;
+    color: white;
+    padding: .5rem;
+    &:focus,
+    &:hover {
+      text-decoration: underline;
+      background-color: white;
+      color: var(--primary);
+    }
+  }
+  a {
+    /* display: block; */
+  }
+  .header__support {
+    padding-left: 1rem;
+    text-align: right;
+    max-width: 250px;
+  }
+`
 
 const Header = () => (
 
@@ -11,33 +49,37 @@ const Header = () => (
             subtitle
             repo
           }
+        },
+        imageAnnertechLogo: file(relativePath: { eq: "annertech.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000 ) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     `}
     render={data => (
-      <header
-        style={{
-          borderBottom: `4px solid var(--primary)`
-        }}
-      >
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.5rem 1rem`,
-          }}
-        >
-          <p style={{ 
-            margin: `0`,
-            }}>
-            <Link to="/">
-              addEventLister
-            <br></br>
-            {data.site.siteMetadata.subtitle}</Link><br></br>
-            <a href="https://annertech.com">A project of Annertech</a> | <a href={data.site.siteMetadata.repo}>Add Event</a>
-          </p>
+      <StyledHeader>
+        <div className="header__inner">
+          <div className="header__branding">
+            <p>
+              <Link className="header__link" to="/">
+                addEventLister
+                <br></br>
+                {data.site.siteMetadata.subtitle}
+              </Link>
+            </p>
+          </div>
+          <div className="header__support">
+              <p>
+                A project of Annertech
+                <a href="https://annertech.com"><Img fluid={data.imageAnnertechLogo.childImageSharp.fluid} /></a>
+              </p>
+            <a className="header__link" href={data.site.siteMetadata.repo}>Add Event</a>
+          </div>
         </div>
-      </header>
+      </StyledHeader>
     )}
   />
 )
